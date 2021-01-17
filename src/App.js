9d6498329752533020815ee09dc4636e5a23e79d
RAVI MAURYA <ravimaurya00563@gmail.com>
2021-01-18 00:17:11 +0530

    Intro to styled Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+// Global Style
+import GlobalStyle from "./components/GlobalStyle";
+
 import AboutUs from "./pages/AboutUs";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Switch>
         <Route path="/" exact component={AboutUs} />
       </Switch>
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,34 +1,67 @@
 import React from "react";
 import home1 from "../img/home1.png";
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>We Work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>true.</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Contact us for any photography or videography ideas.We have
           professional with amazing skills.
         </p>
         <button>Contacts Us</button>
-      </div>
-      <div className="image">
+      </Description>
+      <Image>
         <img src={home1} alt="Guy with camera here" />
-      </div>
-    </div>
+      </Image>
+    </About>
   );
 };
+
+// Styled Components
+const About = styled.div`
+  min-height: 90vh;
+  max-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  /* padding-left: 0rem; */
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
